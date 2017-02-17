5a1900b3328129876d042ad818ad73178faac2fa
tadashiy1012 <yama3ki.tada4@gmail.com>
2017-02-17 19:25:58 +0900

    10th commit

--- application/controllers/Hello.php
+++ application/controllers/Hello.php
@@ -3,7 +3,12 @@ class Hello extends CI_Controller {
 
     public function index()
     {
-        echo 'Hello World!';
+        //echo 'Hello World!';
+        $data = array(
+            'title' => 'hello',
+            'message' => 'Hello World'
+        );
+        $this->load->view('helloview', $data);
     }
 
 }
