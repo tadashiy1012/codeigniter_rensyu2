67bba3cfed4695809a442bcfcc9eb9b59d920c34
tadashiy1012 <yama3ki.tada4@gmail.com>
2017-02-19 17:13:45 +0900

    26th commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:3000';
+$config['base_url'] = 'http://localhost:3000/~vagrant/codeigniter_rensyu2';
 
 /*
 |--------------------------------------------------------------------------
