3824cf0d7da39f4d23c244fb41135593cf03d799
tadashiy1012 <yama3ki.tada4@gmail.com>
2017-02-19 17:12:25 +0900

    25th commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost:3000';
 
 /*
 |--------------------------------------------------------------------------
@@ -521,4 +521,3 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-$config['base_url'] = 'http://localhost:3000'
