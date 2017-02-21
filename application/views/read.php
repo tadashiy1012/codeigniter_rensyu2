7338d1fba18e42f55bbd00e7e89151e98bdd9270
tadashiy1012 <yama3ki.tada4@gmail.com>
2017-02-21 21:50:41 +0900

    49th commit

--- application/views/read.php
+++ application/views/read.php
@@ -1,4 +1,4 @@
 <ul>
-  <li>id:<span><?php echo $item['id']; ?></span></li>
-  <li>name:<span><?php echo $item['name']; ?></span></li>
+  <li>id:<span><?php echo $item[0]->id; ?></span></li>
+  <li>name:<span><?php echo $item[0]->name; ?></span></li>
 </ul>
